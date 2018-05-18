72dee45607168742cd84a43b3eb73bc64496c811
Michelle Olivieri <michelleo72@gmail.com>
2018-05-18 16:22:57 +0000

    automatically backed up by learn

--- app/controllers/landmarks_controller.rb
+++ app/controllers/landmarks_controller.rb
@@ -6,10 +6,24 @@ class LandmarksController < ApplicationController
   end
 
   get '/landmarks/new' do
+    @figures = Figure.all
     erb :'/landmarks/new'
   end
 
   post '/landmarks' do
 
   end
+
+  get '/landmarks/:id' do
+
+  end
+
+  get '/landmarks/:id/edit' do
+
+  end
+
+  patch '/landmarks/:id' do
+
+  end
+  
 end
