2a51136edbe5cba6785bedeed099e9f374804d68
Michelle Olivieri <michelleo72@gmail.com>
2018-05-18 13:10:50 +0000

    automatically backed up by learn

--- app/controllers/figures_controller.rb
+++ app/controllers/figures_controller.rb
@@ -1,10 +1,13 @@
 class FiguresController < ApplicationController
 
   get '/figures' do
-
+    @figures = Figure.all
+    erb :'/figures/index'
   end
 
   get '/figures/new' do
+    @titles = Title.all
+    @landmarks = Landmark.all
     erb :'/figures/new'
   end
 
