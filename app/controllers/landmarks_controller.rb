cf1ca7c8b5bd60c6da66558cffc7751bb9c96ab7
Michelle Olivieri <michelleo72@gmail.com>
2018-05-18 16:21:30 +0000

    automatically backed up by learn

--- app/controllers/landmarks_controller.rb
+++ app/controllers/landmarks_controller.rb
@@ -1,6 +1,8 @@
 class LandmarksController < ApplicationController
-  get '/landmarks' do
 
+  get '/landmarks' do
+    @landmarks = Landmark.all
+    erb '/landmarks/index'
   end
 
   get '/landmarks/new' do
